0c74ee94da05d4aadb9aafb93b2b4a82d3552148
catlair <catlair@qq.com>
2020-12-21 13:36:37 +0800

    fix 所有输出时间一致

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -10,19 +10,18 @@ import { TaskModule } from '../config/globalVar';
 const _log = console.log;
 
 function warpLog() {
-  const ISO_TIME = new Date().toISOString().match(/\w{2}:\w{2}:\w{2}/)[0],
-    HOURS = Number(ISO_TIME.split(':')[0]) + 8,
-    bjHoursNum = HOURS > 24 ? HOURS - 24 : HOURS < 0 ? HOURS + 24 : HOURS,
-    bjHoursStr = bjHoursNum.toString(),
-    bjHours = bjHoursStr.length === 1 ? '0' + bjHoursStr : bjHoursStr,
-    TIME = `[${bjHours + ISO_TIME.replace(/^\w{2}/, '')}] `;
-
   return (message?: any, ...optionalParams: any[]): void => {
-    _log(message, ...optionalParams);
     let msgStr = message;
     for (const m of optionalParams) {
       msgStr += m;
     }
+    const ISO_TIME = new Date().toISOString().match(/\w{2}:\w{2}:\w{2}/)[0],
+      HOURS = Number(ISO_TIME.split(':')[0]) + 8,
+      bjHoursNum = HOURS > 24 ? HOURS - 24 : HOURS < 0 ? HOURS + 24 : HOURS,
+      bjHoursStr = bjHoursNum.toString(),
+      bjHours = bjHoursStr.length === 1 ? '0' + bjHoursStr : bjHoursStr,
+      TIME = `[${bjHours + ISO_TIME.replace(/^\w{2}/, '')}] `;
+    _log(TIME, message, ...optionalParams);
     TaskModule.appInfo += TIME + msgStr + '\n';
   };
 }
