18fc3edbf589d2d4427835c9ac4e0b4baa38f32f
catlair <catlair@qq.com>
2021-01-20 19:09:19 +0800

    提供action/本地使用多用户运行方式 #12

---
+++ tools/bootstrap.js
@@ -0,0 +1,106 @@
+const { writeFileSync } = require('fs');
+const { resolve } = require('path');
+const json = require('../config/config.temp.json');
+const updateSLS = require('./updateSLS');
+const cp = require('child_process');
+
+/**
+ * 提取一个配置
+ */
+function baseConfig(el) {
+  function setMessage() {
+    let message = el.message;
+    const dMsg = json.message;
+    if (!dMsg) return;
+    if (!message) return;
+    if (message === true) {
+      el.message = dMsg;
+      return;
+    }
+    if (message.email === true) {
+      message.email = dMsg.email;
+    }
+    if (message.serverChan === true) {
+      message.serverChan = dMsg.serverChan;
+    }
+    el.message = message;
+  }
+
+  setMessage();
+
+  writeFileSync(
+    resolve(process.cwd(), 'dist/config/config.json'),
+    //格式化输出
+    JSON.stringify(el, null, '\t')
+  );
+}
+
+function scfConfig(el) {
+  const sls = el.sls;
+  new updateSLS()
+    .openJuryVote(el.function?.judgement)
+    .randomDailyRunTime(el.dailyRunTime)
+    .randomJuryRunTime()
+    .updateDescription(sls?.description)
+    .updateRegion(sls?.region)
+    .updateComponentAppName(sls?.appName)
+    .updateSCFName(sls?.name)
+    .update(`serverless.yaml`);
+
+  delete el.sls;
+}
+
+function scfDeploy(sls) {
+  console.log('开始部署', sls?.name);
+  console.log('地区', sls?.region);
+  console.log('描述', sls?.description);
+  try {
+    let stdout = cp.execSync('npm run deploy');
+    console.log(stdout.toString());
+    console.log('部署成功\n\n');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(async () => {
+  if (process.argv.includes('--scf')) {
+    json?.account.forEach(el => {
+      const sls = JSON.parse(JSON.stringify(el.sls));
+      scfConfig(el);
+      baseConfig(el);
+      scfDeploy(sls);
+    });
+    return;
+  }
+
+  if (process.argv.includes('--start')) {
+    const { sendMessage } = require('../dist/utils');
+    let message = '',
+      errorCount = 0;
+    json?.account.forEach(el => {
+      //多用户不再单发请求
+      delete el.message;
+      baseConfig(el);
+      console.log('开始运行\n');
+      try {
+        let stdout = cp.execSync('npm run start');
+        let out = stdout.toString();
+        console.log(out);
+        message += out + '\n\n';
+        console.log('运行成功\n\n');
+      } catch (error) {
+        console.error(error);
+        message += error + '\n\n';
+        errorCount++;
+        console.log('\n\n');
+      }
+    });
+    if (errorCount > 1) {
+      sendMessage(`bili任务存在${errorCount}个错误`, message);
+      return;
+    }
+    sendMessage('bili每日任务完成', message);
+    return;
+  }
+})();
--- tools/muilt-user.js
+++ tools/muilt-user.js
@@ -1,58 +1,52 @@
-const cp = require('child_process');
 const { writeFileSync } = require('fs');
 const { resolve } = require('path');
 const json = require('../config/config.temp.json');
 const updateSLS = require('./updateSLS');
 
-json?.account.forEach(el => {
-  const sls = JSON.parse(JSON.stringify(el.sls));
+function runMuiltUser(callback, ...arg) {
+  json?.account.forEach(el => {
+    const sls = JSON.parse(JSON.stringify(el.sls));
 
-  new updateSLS()
-    .openJuryVote(el.function?.judgement)
-    .randomDailyRunTime(el.dailyRunTime)
-    .randomJuryRunTime()
-    .updateDescription(sls?.description)
-    .updateRegion(sls?.region)
-    .updateComponentAppName(sls?.appName)
-    .updateSCFName(sls?.name)
-    .update(`serverless.yaml`);
+    new updateSLS()
+      .openJuryVote(el.function?.judgement)
+      .randomDailyRunTime(el.dailyRunTime)
+      .randomJuryRunTime()
+      .updateDescription(sls?.description)
+      .updateRegion(sls?.region)
+      .updateComponentAppName(sls?.appName)
+      .updateSCFName(sls?.name)
+      .update(`serverless.yaml`);
 
-  delete el.sls;
+    delete el.sls;
 
-  function setMessage() {
-    let message = el.message;
-    const dMsg = json.message;
-    if (!dMsg) return;
-    if (!message) return;
-    if (message === true) {
-      el.message = dMsg;
-      return;
+    function setMessage() {
+      let message = el.message;
+      const dMsg = json.message;
+      if (!dMsg) return;
+      if (!message) return;
+      if (message === true) {
+        el.message = dMsg;
+        return;
+      }
+      if (message.email === true) {
+        message.email = dMsg.email;
+      }
+      if (message.serverChan === true) {
+        message.serverChan = dMsg.serverChan;
+      }
+      el.message = message;
     }
-    if (message.email === true) {
-      message.email = dMsg.email;
-    }
-    if (message.serverChan === true) {
-      message.serverChan = dMsg.serverChan;
-    }
-    el.message = message;
-  }
 
-  setMessage();
+    setMessage();
 
-  writeFileSync(
-    resolve(process.cwd(), 'dist/config/config.json'),
-    //格式化输出
-    JSON.stringify(el, null, '\t')
-  );
+    writeFileSync(
+      resolve(process.cwd(), 'dist/config/config.json'),
+      //格式化输出
+      JSON.stringify(el, null, '\t')
+    );
 
-  console.log('开始部署', sls?.name);
-  console.log('地区', sls?.region);
-  console.log('描述', sls?.description);
-  try {
-    let stdout = cp.execSync('npm run deploy');
-    console.log(stdout.toString());
-    console.log('部署成功\n\n');
-  } catch (error) {
-    console.error(error);
-  }
-});
+    callback(sls, ...arg);
+  });
+}
+
+module.exports = runMuiltUser;
