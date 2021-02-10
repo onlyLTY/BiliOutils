b2fa6bfaf4795a0e658c06169d084c3e11e94696
catlair <catlair@qq.com>
2021-02-10 21:22:03 +0800

    信息更新

--- src/net/index.ts
+++ src/net/index.ts
@@ -7,8 +7,10 @@ axios.defaults.withCredentials = true;
 
 axios.defaults.headers['User-Agent'] =
   TaskConfig.USER_AGENT ||
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36';
 
 axios.defaults.headers['content-type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers['accept-language'] =
+  'accept-language: zh-CN,zh;q=0.9,en-GB;q=0.8,en;q=0.7';
 
 export default axios;
