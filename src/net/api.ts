b851621735147a0853d27a377b6da181b74d9314
catlair <catlair@qq.com>
2021-07-09 13:48:09 +0800

    feat: 从axios处判断账号是否登录

--- src/net/api.ts
+++ src/net/api.ts
@@ -1,14 +1,33 @@
 import axios from './index';
-import { TaskConfig } from '../config/globalVar';
+import { TaskConfig, TaskModule } from '../config/globalVar';
 import getCookie from '../utils/cookie';
+import { ErrorCodeCommon as ErrCC } from '../config/ErrorCode';
+import { sendMessage } from '../utils';
 
-const res = (res) => {
+const res = res => {
+  // 设置 cookie 的变化
   const setCookie = res.headers?.['set-cookie'] || [];
   TaskConfig.COOKIE = getCookie(TaskConfig.COOKIE, setCookie);
-  return res;
+
+  // 结果异常检测
+  const code = res.data?.code;
+  const exit = async () => {
+    console.log('运行结束：', ErrCC[code]);
+    // 发送信息
+    await sendMessage('bili每日任务出现异常', TaskModule.appInfo);
+    // 结束进程
+    process.exit(code);
+  };
+  switch (code) {
+    case ErrCC['账号未登录']:
+    case ErrCC['账号被封停']:
+      exit();
+    default:
+      return res;
+  }
 };
 
-const err = (err) => {
+const err = err => {
   return Promise.reject(err);
 };
 
