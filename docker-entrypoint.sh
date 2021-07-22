3fbfa40c001bb098a16e66c6e72fedf9438c40b2
catlair <catlair@qq.com>
2021-07-22 13:41:44 +0800

    fix: docker 命令不正确且缺少依赖

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,15 @@ set -e
 
 node tools/processConfig.js
 
+if [ "$3" == "--scf" ]
+then
+  npm install -g serverless modclean
+fi
+
+if [ "$3" == "deploy:muilt" ]
+then
+  npm install -g serverless modclean
+  node tools/bootstrap.js --scf
+fi
+
 exec "$@"
