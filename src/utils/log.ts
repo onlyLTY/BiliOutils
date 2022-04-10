29859114dca50c39d18ea11131fd06e3beff5ad6
catlair <catlair@qq.com>
2022-04-10 20:45:23 +0800

    revert(logger): winston
    Winston failed to package rollup and fell back to using console directly
    
    #55

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,31 +1,56 @@
 import { getPRCDate } from './pure';
-import * as winston from 'winston';
 
-function formatTime(isoStr: string) {
-  return isoStr.match(/\w{2}:\w{2}:\w{2}/)[0];
-}
+type MessageType = string | number | boolean | undefined | null;
+type LevelType = 'error' | 'warn' | 'info' | 'verbose' | 'debug';
 
 export const LogMessage = {
   value: '',
 };
 
-export const logger = winston.createLogger({
-  level: 'debug',
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp({
-      format: () => getPRCDate().toString(),
-    }),
-  ),
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.printf(({ message, timestamp, level }) => {
-        const msgStr = `[${formatTime(timestamp)}] ${message}`;
-        if (!['debug', 'verbose'].includes(level)) {
-          LogMessage.value += msgStr + '\n';
-        }
-        return msgStr;
-      }),
-    }),
-  ],
-});
+export const logger = {
+  log,
+  error: errorLogger,
+  warn: warnLogger,
+  info: infoLogger,
+  verbose: verboseLogger,
+  debug: debugLogger,
+};
+
+function formatTime(isoStr: string) {
+  return isoStr.match(/\w{2}:\w{2}:\w{2}/)[0];
+}
+
+function getLevelValues(level: LevelType = 'debug') {
+  const LEVEL_VALUE = ['error', 'warn', 'info', 'verbose', 'debug'];
+  const levelIndex = LEVEL_VALUE.indexOf(level);
+  return levelIndex > 0 ? LEVEL_VALUE.slice(0, levelIndex + 1) : LEVEL_VALUE;
+}
+
+function log(level: LevelType, message: MessageType) {
+  const time = formatTime(getPRCDate().toString());
+  if (getLevelValues().includes(level)) {
+    const msgStr = `[${time}] ${message}`;
+    LogMessage.value += msgStr + '\n';
+  }
+  console.log('[%s %s] %s', level, time, message);
+}
+
+function errorLogger(message: MessageType) {
+  log('error', message);
+}
+
+function warnLogger(message: MessageType) {
+  log('warn', message);
+}
+
+function infoLogger(message: MessageType) {
+  log('info', message);
+}
+
+function verboseLogger(message: MessageType) {
+  log('verbose', message);
+}
+
+function debugLogger(message: MessageType) {
+  log('debug', message);
+}
