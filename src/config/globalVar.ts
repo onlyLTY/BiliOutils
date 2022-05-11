466bbe2047f9bffa5209313269bf01bd7ff046a7
KudouRan <kudou.ran@outlook.com>
2022-05-11 15:14:42 +0800

    chore: 分组配置

--- src/config/globalVar.ts
+++ src/config/globalVar.ts
@@ -1,4 +1,6 @@
 import type { Config } from '../types';
+import { LOTTERY_EXCLUDE, LOTTERY_INCLUDE, LOTTERY_UP_BLACKLIST } from '../constant';
+import { getNewObject } from '../utils/pure';
 import { getUserId, getBiliJct } from '../utils/cookie';
 import { checkConfig, getConfig } from './setConfig';
 
@@ -17,8 +19,6 @@ class TaskConfigTemplate {
   COOKIE: string;
   /** bilibili账号的jct */
   readonly BILIJCT: string;
-  /** bilibili 用户昵称 */
-  NICKNAME: string;
   /**操作用户的bilibili mid */
   readonly USERID: number;
   /**【可选】用户代理(浏览器) */
@@ -53,30 +53,58 @@ class TaskConfigTemplate {
   readonly MATCH_DIFF: number;
   /** 自定义推送 Api */
   readonly MESSAGE_API: string;
+  /** 天选屏蔽奖品 */
+  readonly LOTTERY_EXCLUDE: string[];
+  /** 天选包含奖品 */
+  readonly LOTTERY_INCLUDE: string[];
+  /** 黑名单 */
+  readonly LOTTERY_UP_BLACKLIST: number[];
+  /** 是否将天选时刻关注 UP 移动到分组 */
+  readonly LOTTERY_MOVE_TAG: string;
+  /** 天选获取的直播页数 */
+  readonly LOTTERY_PAGE_NUM: number;
 
   constructor(config: Config) {
     this.config = config;
     this.COOKIE = config.cookie;
+
     this.BILIJCT = getBiliJct(config.cookie);
-    this.NICKNAME = '';
     this.USERID = getUserId(config.cookie);
     this.USER_AGENT = config.userAgent;
-    this.BILI_TARGET_COINS = config.targetCoins ?? 5;
     const apiDelay = config.apiDelay ?? [2, 6];
     this.BILI_API_DELAY = Array.isArray(apiDelay) ? strArr2numArr(apiDelay) : [Number(apiDelay)];
-    this.BILI_CUSTOMIZE_UP = strArr2numArr(config.customizeUp) || [];
-    this.BILI_GIFT_UP = strArr2numArr(config.giftUp) || this.BILI_CUSTOMIZE_UP || [];
-    this.BILI_TARGET_LEVEL = config.targetLevel ?? 6;
-    this.BILI_STAY_COINS = config.stayCoins ?? 0;
-    this.BILI_UPPER_ACC_MATCH = config.upperAccMatch || true;
-    this.BILI_COIN_RETRY_NUM = config.coinRetryNum || 4;
-    this.CHARGE_ID = config.chargeUpId || this.USERID;
-    this.CHARGE_PRESET_TIME = config.chargePresetTime || 31;
-    this.PUSHPLUS_TOKEN = process.env.PUSHPLUS_TOKEN || config.message?.pushplusToken;
-    this.MATCH_COINS = config.matchCoins ?? 5;
-    this.MATCH_SELECTION = config.matchSelection ?? 1;
-    this.MATCH_DIFF = config.matchDiff ?? 0;
-    this.MESSAGE_API = config.message?.api;
+
+    const message = getNewObject(config.message);
+    this.MESSAGE_API = message.api;
+    this.PUSHPLUS_TOKEN = message.pushplusToken || process.env.PUSHPLUS_TOKEN;
+
+    const coin = getNewObject(config.coin);
+    this.BILI_CUSTOMIZE_UP = strArr2numArr(coin.customizeUp || config.customizeUp) || [];
+    this.BILI_UPPER_ACC_MATCH = coin.upperAccMatch || config.upperAccMatch || true;
+    this.BILI_COIN_RETRY_NUM = coin.retryNum || config.coinRetryNum || 4;
+    this.BILI_TARGET_LEVEL = (coin.targetLevel || config.targetLevel) ?? 6;
+    this.BILI_STAY_COINS = (coin.stayCoins || config.stayCoins) ?? 0;
+    this.BILI_TARGET_COINS = (coin.targetCoins || config.targetCoins) ?? 5;
+
+    const gift = getNewObject(config.gift);
+    this.BILI_GIFT_UP = strArr2numArr(gift.mids || config.giftUp) || this.BILI_CUSTOMIZE_UP || [];
+
+    const charge = getNewObject(config.charge);
+    this.CHARGE_ID = charge.mid || config.chargeUpId || this.USERID;
+    this.CHARGE_PRESET_TIME = charge.presetTime || config.chargePresetTime || 31;
+
+    const match = getNewObject(config.match);
+    this.MATCH_COINS = (match.coins || config.matchCoins) ?? 5;
+    this.MATCH_SELECTION = (match.selection || config.matchSelection) ?? 1;
+    this.MATCH_DIFF = (match.diff || config.matchDiff) ?? 0;
+
+    // 最新的就不再有兼容了
+    const lottery = getNewObject(config.lottery);
+    this.LOTTERY_EXCLUDE = lottery.excludeAward || LOTTERY_EXCLUDE;
+    this.LOTTERY_INCLUDE = lottery.includeAward || LOTTERY_INCLUDE;
+    this.LOTTERY_UP_BLACKLIST = lottery.blackUid || LOTTERY_UP_BLACKLIST;
+    this.LOTTERY_MOVE_TAG = lottery.moveTag;
+    this.LOTTERY_PAGE_NUM = lottery.pageNum || 2;
   }
 }
 
@@ -115,6 +143,10 @@ export abstract class TaskModule {
   static vipType = 0;
   /** 充电留言 token */
   static chargeOrderNo: string;
+  /** 用户等级 */
+  static userLevel = 0;
+  /** 用户昵称 */
+  static nickname: string;
 }
 
 export function initialize(config?: Config) {
