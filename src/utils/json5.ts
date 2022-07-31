d42677ab150998f11b3e74d1123766069e2f5b47
KudouRan <kudou.ran@outlook.com>
2022-07-31 20:12:53 +0800

    feat: json5 -> golden-fleece

--- src/utils/json5.ts
+++ src/utils/json5.ts
@@ -1,14 +1,36 @@
-import * as json5 from 'json5';
+import * as fleece from 'golden-fleece';
 
-interface JSON5Type {
-  parse<T = any>(text: string, reviver?: ((this: any, key: string, value: any) => any) | null): T;
-  stringify(
-    value: any,
-    replacer?: ((this: any, key: string, value: any) => any) | null,
-    space?: string | number | null,
-  ): string;
+export class JSON5 {
+  static parse(str: string) {
+    return fleece.evaluate(str);
+  }
+
+  static stringify(obj: Record<string, any>) {
+    return fleece.stringify(obj, {
+      spaces: 2,
+      singleQuotes: true,
+    });
+  }
+
+  static patch(str: string, patch: Record<string, any>) {
+    return fleece.patch(str, {
+      ...this.parse(str),
+      ...patch,
+    });
+  }
 }
 
-// 解决cjs mjs环境导致的路径问题
-// @ts-ignore
-export const JSON5: JSON5Type = json5.default ? json5.default : json5;
+// import * as json5 from 'json5';
+
+// interface JSON5Type {
+//   parse<T = any>(text: string, reviver?: ((this: any, key: string, value: any) => any) | null): T;
+//   stringify(
+//     value: any,
+//     replacer?: ((this: any, key: string, value: any) => any) | null,
+//     space?: string | number | null,
+//   ): string;
+// }
+
+// // 解决cjs mjs环境导致的路径问题
+// // @ts-ignore
+// export const JSON5: JSON5Type = json5.default ? json5.default : json5;
